25d984bd613c428c51f3a2646b3a1027d1ce6b0a
/ERROR <52045774+andrihermawan1997@users.noreply.github.com>
2021-12-18 14:31:05 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   owners: [795708124442918913],
   nodes: [{
     identifier: 'Haruka Music',
-    host: 'lavalink-repl.parasop.repl.co',
-    password: 'youshallnotpass',
+    host: 'lavalink.eu',
+    password: 'Raccoon',
     port: 443,
     secure: true,
   }],
